b17a02d29f385c65e12fed78502f00ea15580004
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-06-11 18:33:54 -0400

    chore: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <link href="/favicon_io/favicon.ico" rel="shortcut icon" />
+        <link href="/favicon_io/site.webmanifest" rel="manifest" />
+        <link
+          href="/favicon_io/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon_io/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon_io/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
