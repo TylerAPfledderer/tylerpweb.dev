a4b5c8d2ad148b73442ce8b541490a8723a69eed
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-06-28 09:49:35 -0400

    chore(pages/index): remove unused imports/declarations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,14 @@
 import { AboutMeSection } from "@/components/AboutMeSection";
 import { HeroSection } from "@/components/HeroSection";
 import { MainSection } from "@/components/MainSection";
-import { socialMediaLinks } from "@/data";
-import { CurvedDownArrow } from "@/svg-icons";
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  Heading,
-  Highlight,
-  Icon,
-  Img,
-  Link,
-  List,
-  ListItem,
-  Stack,
-  Text,
-  VStack,
-  keyframes,
-} from "@chakra-ui/react";
-
-const waggle = keyframes({
-  "10%": {
-    transform: "scale(0.8) rotate(-20deg)",
-  },
-  "40%": {
-    transform: "scale(1.9) rotate(10deg)",
-  },
-  "60%": {
-    transform: "scale(1.6) rotate(-10deg)",
-  },
-  "100%": {
-    transform: "scale(1.4)",
-  },
-});
+import { SkillsSection } from "@/components/SkillsSection";
+import { Box, Center, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
       <Center as="header">
-        <Box maxW="8xl" w="full" px={8} py={6}>
+        <Box maxW="8xl" w="full" px="8" py="6">
           <Text as="span" fontFamily="heading" fontSize="xl">
             TP
           </Text>
